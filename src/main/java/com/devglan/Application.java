52b99f43399a100a0f0860ffcd0a99d4ee84df34
Dhiraj Prakash <dhiraj@brazhuman.com>
2018-11-10 17:49:10 -0200

    Making war from spring boot jar

--- src/main/java/com/devglan/Application.java
+++ src/main/java/com/devglan/Application.java
@@ -2,12 +2,21 @@ package com.devglan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+  //added to create war
+    @Override
+  	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  			return application.sources(Application.class);
+  		}
 
 }
